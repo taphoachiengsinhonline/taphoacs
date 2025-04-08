adc87bd35600339464a7bc4394300f6f655f3f05
taphoachiengsinhonline <maytinhthaikhang@gmail.com>
2025-04-09 04:27:24 +0700

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,1 +1,10 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: { type: String, unique: true },
+  password: String, // bạn có thể hash sau này
+});
+
+module.exports = mongoose.model('User', userSchema);
 
