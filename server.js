2af21931484cdbac6357bbd51ca9b96a5cbb6b5f
taphoachiengsinhonline <maytinhthaikhang@gmail.com>
2025-04-09 03:34:38 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-
+const Product = require('./models/Product'); // nếu cùng thư mục thì sửa lại path tương ứng
 const app = express();
 app.use(cors());
 app.use(express.json());
