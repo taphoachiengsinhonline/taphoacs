ffe25a0daaa60ab71baebb124bc857a949ff2b16
taphoachiengsinhonline <maytinhthaikhang@gmail.com>
2025-04-09 06:33:30 +0700

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -8,6 +8,14 @@ const productSchema = new mongoose.Schema({
   price: {
     type: Number,
     required: true
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: String,
+    required: true
   }
 });
 
