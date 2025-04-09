cf181aabd28dc1442f94f2667482d0fd9b1231d6
taphoachiengsinhonline <maytinhthaikhang@gmail.com>
2025-04-09 15:04:05 +0700

    Update Category.js

--- models/Category.js
+++ models/Category.js
@@ -1,11 +1,8 @@
 const mongoose = require('mongoose');
 
 const categorySchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    unique: true,
-  },
+  name: { type: String, required: true, unique: true, },
+  parent: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', default: null } // danh mục cha
 });
 
 module.exports = mongoose.model('Category', categorySchema);
