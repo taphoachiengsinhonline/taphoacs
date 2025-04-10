1e0f28e7c1edb6e4e6f5d6e4383ef251ac55da39
taphoachiengsinhonline <maytinhthaikhang@gmail.com>
2025-04-10 17:43:13 +0700

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -7,19 +7,16 @@ const Category = require('../models/Category');
 router.get('/products', async (req, res) => {
   try {
     const { category } = req.query;
-    let filter = {};
 
+    let filter = {};
     if (category && category !== 'Tất cả') {
-      const foundCategory = await Category.findOne({ name: category });
-      if (!foundCategory) {
-        return res.status(404).json({ error: 'Danh mục không tồn tại' });
-      }
-      filter.category = foundCategory._id;
+      filter.category = category; // category là _id dạng chuỗi
     }
 
-    const products = await Product.find(filter).populate('category'); // để client thấy tên danh mục
+    const products = await Product.find(filter);
     res.json(products);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 });
+
