1a03125b76e9f3e61631ed84caafcdbf6a5973c2
taphoachiengsinhonline <maytinhthaikhang@gmail.com>
2025-04-10 14:27:53 +0700

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,1 +1,8 @@
+exports.isAdminMiddleware = (req, res, next) => {
+  const user = req.user; // assume đã gán user từ token hoặc session
+  if (!user || !user.isAdmin) {
+    return res.status(403).json({ message: 'Chỉ admin mới có quyền thực hiện thao tác này.' });
+  }
+  next();
+};
 
