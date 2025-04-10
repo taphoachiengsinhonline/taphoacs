d34661c4dea57617964c4beac75325bc65225d64
taphoachiengsinhonline <maytinhthaikhang@gmail.com>
2025-04-10 17:21:27 +0700

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,25 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const Product = require('../models/Product');
-const { isAdminMiddleware } = require('../middleware/authMiddleware'); // bạn cần có middleware này
 
-router.post('/products', isAdminMiddleware, async (req, res) => {
-  try {
-    const { name, category, price, stock, attributes } = req.body;
-    const newProduct = new Product({ name, category, price, stock, attributes });
-    await newProduct.save();
-    res.status(201).json(newProduct);
-  } catch (err) {
-    res.status(400).json({ error: err.message });
-  }
-});
-// Lấy tất cả sản phẩm, có thể lọc theo danh mục
+// GET all products hoặc theo category
 router.get('/products', async (req, res) => {
   try {
     const { category } = req.query;
     let filter = {};
-
-    if (category && category !== 'Tất cả') {
+    if (category) {
       filter.category = category;
     }
 
