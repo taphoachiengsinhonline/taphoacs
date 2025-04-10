1c76fc0cc5cd68f96e870a7cf79f4b3b0d885bd2
taphoachiengsinhonline <maytinhthaikhang@gmail.com>
2025-04-10 14:14:52 +0700

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -1,22 +1,21 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  price: {
-    type: Number,
-    required: true
-  },
-  image: {
-    type: String,
-    required: true
-  },
-  category: {
-    type: String,
-    required: true
-  },
+  name: { type: String, required: true },
+  category: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true },
+  price: { type: Number, required: true }, // Giá mặc định
+  stock: { type: Number, required: true },
+  attributes: [
+    {
+      name: { type: String, required: true }, // VD: "Dung lượng", "Size"
+      options: [
+        {
+          value: { type: String, required: true }, // VD: "4GB", "8GB"
+          price: { type: Number, required: true }  // Giá riêng cho mỗi lựa chọn
+        }
+      ]
+    }
+  ]
 });
 
 module.exports = mongoose.model('Product', productSchema);
