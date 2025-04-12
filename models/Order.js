549dbb03fcf36b161c28f951a8ff3e2afbf901bf
taphoachiengsinhonline <maytinhthaikhang@gmail.com>
2025-04-13 01:25:03 +0700

    Update Order.js

--- models/Order.js
+++ models/Order.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
   user: { type: String, required: true },
+  phone: String,
+  shippingAddress: String,
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User'},
   items: [{ name: String, price: Number, quantity: Number }],
   total: { type: Number, required: true },
   createdAt: { type: Date, default: Date.now }
