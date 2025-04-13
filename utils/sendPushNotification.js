0289a217de62751b987757617b37d1de83732709
taphoachiengsinhonline <maytinhthaikhang@gmail.com>
2025-04-14 02:29:43 +0700

    Update sendPushNotification.js

--- utils/sendPushNotification.js
+++ utils/sendPushNotification.js
@@ -1,1 +1,16 @@
+const axios = require('axios');
 
+const sendPushNotification = async (expoPushToken, title, body) => {
+  try {
+    await axios.post('https://exp.host/--/api/v2/push/send', {
+      to: expoPushToken,
+      sound: 'default',
+      title,
+      body,
+    });
+  } catch (error) {
+    console.error('Lỗi gửi push notification:', error.message);
+  }
+};
+
+module.exports = sendPushNotification;
