1903383e8fed7aaf04c89516e2c4cab0c03639bc
taphoachiengsinhonline <maytinhthaikhang@gmail.com>
2025-04-13 14:13:32 +0700

    Update getPushToken.js

--- utils/getPushToken.js
+++ utils/getPushToken.js
@@ -1,1 +1,28 @@
+// utils/getPushToken.js
+import * as Notifications from 'expo-notifications';
+import * as Device from 'expo-device';
+import { Platform } from 'react-native';
+
+export async function registerForPushNotificationsAsync() {
+  if (Device.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+
+    if (finalStatus !== 'granted') {
+      alert('Không cấp quyền thông báo!');
+      return null;
+    }
+
+    const tokenData = await Notifications.getExpoPushTokenAsync();
+    return tokenData.data;
+  } else {
+    alert('Phải dùng thiết bị thật để nhận thông báo');
+    return null;
+  }
+}
 
