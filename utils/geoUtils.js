e7ce465d320ee9fbe983ed3d2c8812c409f1a281
taphoachiengsinhonline <maytinhthaikhang@gmail.com>
2025-05-21 23:19:22 +0700

    Update geoUtils.js

--- utils/geoUtils.js
+++ utils/geoUtils.js
@@ -1,1 +1,20 @@
-
+exports.findNearestStaff = async (orderLocation) => {
+  return await User.aggregate([
+    {
+      $geoNear: {
+        near: {
+          type: 'Point',
+          coordinates: orderLocation
+        },
+        distanceField: 'distance',
+        maxDistance: 5000, // 5km
+        query: {
+          'deliveryInfo.status': 'available',
+          role: 'staff'
+        },
+        spherical: true
+      }
+    },
+    { $limit: 5 }
+  ]);
+};
