c9eba95f6323410f2b148e4e2ecad1dc8eff7235
taphoachiengsinhonline <maytinhthaikhang@gmail.com>
2025-05-21 23:13:59 +0700

    Update isStaffMiddleware.js

--- middlewares/isStaffMiddleware.js
+++ middlewares/isStaffMiddleware.js
@@ -1,1 +1,9 @@
-
+// middlewares/isStaffMiddleware.js
+exports.isStaffMiddleware = (req, res, next) => {
+  if (req.user.role !== 'staff') {
+    return res.status(403).json({
+      message: 'Yêu cầu quyền nhân viên giao hàng'
+    });
+  }
+  next();
+};
