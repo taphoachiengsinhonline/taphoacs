74850c342cf17bd2df3b987d1b2f2ab73755ad31
taphoachiengsinhonline <maytinhthaikhang@gmail.com>
2025-05-24 17:01:13 +0700

    Update authMiddleware.js

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -42,3 +42,12 @@ exports.isAdminMiddleware = (req, res, next) => {
   }
   next();
 };
+
+exports.isAdmin = (req, res, next) => {
+  if (req.user.role !== 'admin') {
+    return res.status(403).json({ message: 'Truy cập bị từ chối' });
+  }
+  next();
+};
+
+
