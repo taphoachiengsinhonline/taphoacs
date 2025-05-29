ca651303c880c6d3e5d54f38298e4853d2e847dc
taphoachiengsinhonline <maytinhthaikhang@gmail.com>
2025-05-29 21:22:33 +0700

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,11 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Client-Type']
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
