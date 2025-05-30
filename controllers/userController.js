87c77c9e677102d266a7824651b99b7699cb17bc
taphoachiengsinhonline <maytinhthaikhang@gmail.com>
2025-05-31 01:18:34 +0700

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,1 +1,27 @@
+const User = require('../models/User');
+
+const updateLocation = async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const { latitude, longitude } = req.body;
+
+    if (!latitude || !longitude) {
+      return res.status(400).json({ message: 'Thiếu tọa độ location' });
+    }
+
+    await User.findByIdAndUpdate(userId, {
+      location: {
+        type: 'Point',
+        coordinates: [longitude, latitude],
+      },
+    });
+
+    res.json({ message: 'Cập nhật vị trí thành công' });
+  } catch (error) {
+    console.error('[UpdateLocation] Lỗi:', error);
+    res.status(500).json({ message: 'Lỗi server' });
+  }
+};
+
+module.exports = { ..., updateLocation };
 
