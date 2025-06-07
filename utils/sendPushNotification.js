e04cbf0a429e9301517d5473e52b61dc61f97bf5
taphoachiengsinhonline <maytinhthaikhang@gmail.com>
2025-06-08 04:00:00 +0700

    Update sendPushNotification.js

--- utils/sendPushNotification.js
+++ utils/sendPushNotification.js
@@ -1,14 +1,23 @@
 const axios = require('axios');
 
-module.exports = async (token, notificationData) => { // ✅ Nhận object data
+module.exports = async (token, notificationData) => {
   try {
     const message = {
       to: token,
       sound: 'default',
-      ...notificationData // ✅ Spread toàn bộ data
+      title: notificationData.title,
+      body: notificationData.body,
+      data: notificationData.data || {}
     };
 
-    const response = await axios.post('https://exp.host/--/api/v2/push/send', message);
+    const response = await axios.post('https://exp.host/--/api/v2/push/send', message, {
+      headers: {
+        'Accept': 'application/json',
+        'Accept-encoding': 'gzip, deflate',
+        'Content-Type': 'application/json',
+      }
+    });
+    
     return response.data;
   } catch (error) {
     console.error('Error sending push notification:', error.response?.data || error.message);
