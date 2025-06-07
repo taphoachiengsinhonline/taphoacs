5dac029ddccf8f89769e61c461f28e3253a87f44
taphoachiengsinhonline <maytinhthaikhang@gmail.com>
2025-06-08 04:08:23 +0700

    Update sendPushNotificationToCustomer.js

--- utils/sendPushNotificationToCustomer.js
+++ utils/sendPushNotificationToCustomer.js
@@ -1,1 +1,27 @@
+const sendPushNotification = require('./sendPushNotification');
+const User = require('../models/User');
 
+module.exports = async (fcmToken, notificationData) => {
+  try {
+    if (!fcmToken) {
+      console.log('Không có FCM token, bỏ qua gửi thông báo');
+      return;
+    }
+    
+    console.log(`Gửi thông báo đến token: ${fcmToken}`);
+    console.log('Nội dung:', notificationData);
+    
+    await sendPushNotification(fcmToken, notificationData);
+  } catch (error) {
+    console.error('Lỗi gửi thông báo cho khách hàng:', error);
+    
+    // Thử xóa token nếu lỗi do token không hợp lệ
+    if (error.response?.data?.errors?.[0]?.code === 'invalid_credentials') {
+      console.log('Token không hợp lệ, xóa token khỏi user');
+      await User.updateOne(
+        { fcmToken },
+        { $unset: { fcmToken: 1 } }
+      );
+    }
+  }
+};
