95905af7613f39d89cdf0c4c01d82862e13213ae
taphoachiengsinhonline <maytinhthaikhang@gmail.com>
2025-06-16 15:23:08 +0700

    Update voucherRoutes.js

--- routes/voucherRoutes.js
+++ routes/voucherRoutes.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const voucherController = require('../controllers/voucherController');
+const { verifyToken, isAdminMiddleware } = require('../middlewares/authMiddleware');
 
+router.get('/', verifyToken, voucherController.getVouchers);
+router.post('/', verifyToken, isAdminMiddleware, voucherController.createVoucher);
+router.post('/apply', verifyToken, voucherController.applyVoucher);
+router.delete('/:id', verifyToken, isAdminMiddleware, voucherController.deleteVoucher);
+
+module.exports = router;
