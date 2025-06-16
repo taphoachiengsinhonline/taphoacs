90300dd96a875667fa7c5191db639118a9775238
taphoachiengsinhonline <maytinhthaikhang@gmail.com>
2025-06-16 15:17:33 +0700

    Update Voucher.js

--- models/Voucher.js
+++ models/Voucher.js
@@ -1,1 +1,34 @@
+const mongoose = require('mongoose');
 
+const voucherSchema = new mongoose.Schema({
+  code: {
+    type: String,
+    required: true,
+    unique: true,
+    uppercase: true,
+    trim: true
+  },
+  type: {
+    type: String,
+    enum: ['shipping'],
+    required: true
+  },
+  discount: {
+    type: Number,
+    required: true,
+    min: 0
+  },
+  expiryDate: {
+    type: Date,
+    required: true
+  },
+  isActive: {
+    type: Boolean,
+    default: true
+  }
+}, {
+  versionKey: false,
+  timestamps: true
+});
+
+module.exports = mongoose.model('Voucher', voucherSchema);
