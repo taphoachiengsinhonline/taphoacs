44e73133fed06fe1015ee27b9d0a6d4ebb8abce0
taphoachiengsinhonline <maytinhthaikhang@gmail.com>
2025-06-16 17:37:04 +0700

    Update Settings.js

--- models/Settings.js
+++ models/Settings.js
@@ -1,1 +1,8 @@
+const mongoose = require('mongoose');
 
+const settingsSchema = new mongoose.Schema({
+  newUserVoucherId: { type: mongoose.Schema.Types.ObjectId, ref: 'Voucher' },
+  newUserVoucherEnabled: { type: Boolean, default: false }
+}, { timestamps: true, versionKey: false });
+
+module.exports = mongoose.model('Settings', settingsSchema);
