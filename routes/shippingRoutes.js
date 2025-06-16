655eb8b02fceafffe6fae89f475ddd07c37653ba
taphoachiengsinhonline <maytinhthaikhang@gmail.com>
2025-06-16 15:22:24 +0700

    Update shippingRoutes.js

--- routes/shippingRoutes.js
+++ routes/shippingRoutes.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const shippingController = require('../controllers/shippingController');
+const { verifyToken, isAdminMiddleware } = require('../middlewares/authMiddleware');
 
+router.get('/fees', verifyToken, shippingController.getShippingFees);
+router.put('/fees', verifyToken, isAdminMiddleware, shippingController.updateShippingFees);
+router.get('/free-ship', verifyToken, shippingController.getFreeShipThreshold);
+router.put('/free-ship', verifyToken, isAdminMiddleware, shippingController.updateFreeShipThreshold);
+
+module.exports = router;
