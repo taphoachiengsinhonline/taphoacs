d0bce4f2cc4f31bc848b82b761d86dd85f596d98
taphoachiengsinhonline <maytinhthaikhang@gmail.com>
2025-06-16 15:15:34 +0700

    Update ShippingFee.js

--- models/ShippingFee.js
+++ models/ShippingFee.js
@@ -1,1 +1,29 @@
+const mongoose = require('mongoose');
 
+const shippingFeeSchema = new mongoose.Schema({
+  ranges: [
+    {
+      maxDistance: {
+        type: Number,
+        required: true,
+        min: 0
+      },
+      fee: {
+        type: Number,
+        required: true,
+        min: 0
+      }
+    }
+  ],
+  freeShipThreshold: {
+    type: Number,
+    required: true,
+    min: 0,
+    default: 0
+  }
+}, {
+  versionKey: false,
+  timestamps: true
+});
+
+module.exports = mongoose.model('ShippingFee', shippingFeeSchema);
