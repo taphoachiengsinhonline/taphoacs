8ed069d7aa3227855bddf3be6a6831cfa66b354d
taphoachiengsinhonline <maytinhthaikhang@gmail.com>
2025-06-18 00:27:37 +0700

    Update UserVoucher.js

--- models/UserVoucher.js
+++ models/UserVoucher.js
@@ -1,11 +1,26 @@
 const mongoose = require('mongoose');
 
 const userVoucherSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  voucher: { type: mongoose.Schema.Types.ObjectId, ref: 'Voucher', required: true },
-  isUsed: { type: Boolean, default: false },
-  usedAt: { type: Date },
-  collectedAt: { type: Date, default: Date.now }
-}, { timestamps: true, versionKey: false });
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  voucher: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Voucher',
+    required: true
+  },
+  isUsed: {
+    type: Boolean,
+    default: false
+  },
+  collectedAt: {
+    type: Date,
+    default: Date.now
+  }
+}, {
+  timestamps: true
+});
 
 module.exports = mongoose.model('UserVoucher', userVoucherSchema);
