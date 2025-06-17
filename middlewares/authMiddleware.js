e2aff9e750dfd480e1225cbd4568bc5dc906e905
taphoachiengsinhonline <maytinhthaikhang@gmail.com>
2025-06-18 01:40:30 +0700

    Update authMiddleware.js

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -32,9 +32,21 @@ const isAdminMiddleware = isAdmin;
 // Thêm verifyAdmin để tương thích với voucherRoutes.js
 const verifyAdmin = isAdmin;
 
+
+const restrictTo = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({ message: 'Bạn không có quyền truy cập' });
+    }
+    next();
+  };
+};
+
+
 module.exports = {
   verifyToken,
   isAdmin,
   isAdminMiddleware,
   verifyAdmin,
+  restrictTo
 };
