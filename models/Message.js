5264fcd0062ff6c336a78db4b1dea2a9837c2752
taphoachiengsinhonline <maytinhthaikhang@gmail.com>
2025-06-19 18:06:50 +0700

    Create Message.js

---
+++ models/Message.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const messageSchema = new mongoose.Schema({
+  conversationId: { type: mongoose.Schema.Types.ObjectId, ref: 'Conversation', required: true },
+  senderId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  content: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now }
+});
+module.exports = mongoose.model('Message', messageSchema);
