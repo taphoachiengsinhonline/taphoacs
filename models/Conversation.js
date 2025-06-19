7a31cd00ddf2cbc1cef29e9e14b4ce8826cf14e6
taphoachiengsinhonline <maytinhthaikhang@gmail.com>
2025-06-19 18:06:16 +0700

    Create Conversation.js

---
+++ models/Conversation.js
@@ -0,0 +1,10 @@
+// models/Conversation.js
+const mongoose = require('mongoose');
+const conversationSchema = new mongoose.Schema({
+  productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+  customerId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  adminId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now }
+});
+module.exports = mongoose.model('Conversation', conversationSchema);
