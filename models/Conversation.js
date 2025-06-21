8f4c3e3354a707e5197fa3e27da20c8b87d8d5ad
taphoachiengsinhonline <maytinhthaikhang@gmail.com>
2025-06-22 04:40:43 +0700

    Update Conversation.js

--- models/Conversation.js
+++ models/Conversation.js
@@ -2,9 +2,21 @@
 const mongoose = require('mongoose');
 
 const ConversationSchema = new mongoose.Schema({
-  productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
-  customerId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  sellerId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, // Thay adminId bằng sellerId, không required
+  productId: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'Product', 
+    required: true 
+  },
+  customerId: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'User', 
+    required: true 
+  },
+  sellerId: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'User',
+    required: true
+  },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
 });
