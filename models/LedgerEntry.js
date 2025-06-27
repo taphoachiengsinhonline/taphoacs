a19c5cc25629f33875f7340ee9188398cb516dc0
taphoachiengsinhonline <maytinhthaikhang@gmail.com>
2025-06-27 15:40:51 +0700

    Create LedgerEntry.js

---
+++ models/LedgerEntry.js
@@ -0,0 +1,35 @@
+// models/LedgerEntry.js
+const mongoose = require('mongoose');
+
+const ledgerEntrySchema = new mongoose.Schema({
+  seller: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  order: { // Giao dịch liên quan đến đơn hàng nào
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Order',
+  },
+  type: { // Loại giao dịch: Ghi có (doanh thu), Ghi nợ (rút tiền/thanh toán)
+    type: String,
+    enum: ['credit', 'debit'],
+    required: true,
+  },
+  amount: { // Số tiền (luôn là số dương)
+    type: Number,
+    required: true,
+  },
+  description: { // Mô tả giao dịch
+    type: String,
+    required: true,
+  },
+  balanceAfter: { // Số dư sau giao dịch
+    type: Number,
+    required: true,
+  }
+}, { timestamps: true });
+
+ledgerEntrySchema.index({ seller: 1, createdAt: -1 });
+
+module.exports = mongoose.model('LedgerEntry', ledgerEntrySchema);
