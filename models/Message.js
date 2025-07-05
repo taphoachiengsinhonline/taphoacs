f6a5ea91742290c4e60f3cc4c8e8b6fd154ceccd
taphoachiengsinhonline <maytinhthaikhang@gmail.com>
2025-07-06 01:00:41 +0700

    Update Message.js

--- models/Message.js
+++ models/Message.js
@@ -3,6 +3,7 @@ const messageSchema = new mongoose.Schema({
   conversationId: { type: mongoose.Schema.Types.ObjectId, ref: 'Conversation', required: true },
   senderId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   content: { type: String, required: true },
+  isRead: { type: Boolean, default: false },
   createdAt: { type: Date, default: Date.now }
 });
 module.exports = mongoose.model('Message', messageSchema);
