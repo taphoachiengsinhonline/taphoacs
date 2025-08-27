2c6a81a074a277d97476aa3e954f07b376119c5c
taphoachiengsinhonline <maytinhthaikhang@gmail.com>
2025-08-28 03:15:48 +0700

    Update sellerController.js

--- controllers/sellerController.js
+++ controllers/sellerController.js
@@ -3,7 +3,6 @@ const Product = require('../models/Product');
 const Order = require('../models/Order');
 const User = require('../models/User');
 const PendingUpdate = require('../models/PendingUpdate');
-const { sendOtpSms } = require('../utils/sms');
 const crypto = require('crypto');
 const moment = require('moment-timezone');
 const Conversation = require('../models/Conversation');
