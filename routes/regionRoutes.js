1044009f78e5d75cb4be4c4a80cd54c326d567ab
taphoachiengsinhonline <maytinhthaikhang@gmail.com>
2025-09-08 00:02:54 +0700

    Create regionRoutes.js

---
+++ routes/regionRoutes.js
@@ -0,0 +1,16 @@
+// File: backend/routes/regionRoutes.js
+const express = require('express');
+const router = express.Router();
+const regionController = require('../controllers/regionController');
+const { verifyToken, isAdmin } = require('../middlewares/authMiddleware');
+
+// Áp dụng middleware cho tất cả các route trong file này
+router.use(verifyToken, isAdmin);
+
+// Định nghĩa các route CRUD
+router.get('/', regionController.getAllRegions);
+router.post('/', regionController.createRegion);
+router.put('/:regionId', regionController.updateRegion);
+router.delete('/:regionId', regionController.deleteRegion);
+
+module.exports = router;
