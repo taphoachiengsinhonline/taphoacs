2f1154cd586f1090361d9ba50e16f69911af212d
taphoachiengsinhonline <maytinhthaikhang@gmail.com>
2025-09-15 00:21:21 +0700

    Update reviewRoutes.js

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -5,6 +5,9 @@ const reviewController = require('../controllers/reviewController');
 const { verifyToken } = require('../middlewares/authMiddleware');
 
 // Chỉ người dùng đã đăng nhập mới có thể tạo review
+router.get('/product/:productId', reviewController.getReviewsForProduct);
+// <<< ROUTE MỚI >>>
+router.get('/status/:orderId', verifyToken, reviewController.getReviewStatusForOrder);
 router.post('/', verifyToken, reviewController.createReview);
 
 module.exports = router;
