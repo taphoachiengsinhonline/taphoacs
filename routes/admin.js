891b30dcdf1ab0c32673aff4986e0de3892c4276
taphoachiengsinhonline <maytinhthaikhang@gmail.com>
2025-09-20 22:34:03 +0700

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -233,10 +233,10 @@ router.get('/dashboard-counts', adminController.getAdminDashboardCounts);
 router.post('/shippers/:shipperId/remind-debt', adminController.remindShipperToPayDebt);
 //router.get('/sellers', verifyToken, verifyRegionManager, adminController.getAllSellers);
 // <<< BẮT ĐẦU THÊM ROUTE CHO QUẢN LÝ VÙNG >>>
-//router.get('/region-managers', verifyToken, verifyAdmin, adminController.getRegionManagers);
-//router.post('/region-managers', verifyToken, verifyAdmin, adminController.createRegionManager);
-router.put('/region-managers/:managerId', verifyToken, verifyAdmin, adminController.updateRegionManager);
-router.put('/users/:userId/assign-manager', verifyToken, verifyAdmin, adminController.assignManagerToUser);
+router.get('/region-managers', verifyToken, isAdmin, adminController.getRegionManagers);
+router.post('/region-managers', verifyToken, isAdmin, adminController.createRegionManager);
+router.put('/region-managers/:managerId', verifyToken, isAdmin, adminController.updateRegionManager);
+router.put('/users/:userId/assign-manager', verifyToken, isAdmin, adminController.assignManagerToUser);
 // <<< KẾT THÚC THÊM ROUTE >>>
 
 module.exports = router;
