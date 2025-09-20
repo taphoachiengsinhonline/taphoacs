f29abbcbd24707c5c8340ae063a5a366afdeeb56
taphoachiengsinhonline <maytinhthaikhang@gmail.com>
2025-09-20 19:46:29 +0700

    Create regionAuthMiddleware.js

---
+++ middlewares/regionAuthMiddleware.js
@@ -0,0 +1,16 @@
+// middlewares/regionAuthMiddleware.js
+
+const verifyRegionManager = (req, res, next) => {
+    if (req.user.role !== 'region_manager' && req.user.role !== 'admin') {
+        return res.status(403).json({ message: 'Không có quyền truy cập.' });
+    }
+    // Nếu là admin thì cho qua luôn
+    if (req.user.role === 'admin') {
+        return next();
+    }
+    // Nếu là region_manager, đảm bảo họ có thông tin khu vực
+    if (!req.user.region) {
+        return res.status(403).json({ message: 'Tài khoản của bạn chưa được gán vào khu vực nào.' });
+    }
+    next();
+};
