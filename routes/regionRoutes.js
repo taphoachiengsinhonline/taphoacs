e8894c5b994cc00a5e5bf7fa71790d4381e6d6dc
taphoachiengsinhonline <maytinhthaikhang@gmail.com>
2025-09-24 23:22:09 +0700

    Update regionRoutes.js

--- routes/regionRoutes.js
+++ routes/regionRoutes.js
@@ -3,12 +3,13 @@ const express = require('express');
 const router = express.Router();
 const regionController = require('../controllers/regionController');
 const { verifyToken, isAdmin } = require('../middlewares/authMiddleware');
+const { verifyRegionManager } = require('../middlewares/regionAuthMiddleware');
 
 // Áp dụng middleware cho tất cả các route trong file này
 router.use(verifyToken, isAdmin);
 
 // Định nghĩa các route CRUD
-router.get('/', regionController.getAllRegions);
+router.get('/', [verifyToken, verifyRegionManager], regionController.getAllRegions);
 router.post('/', regionController.createRegion);
 router.put('/:regionId', regionController.updateRegion);
 router.delete('/:regionId', regionController.deleteRegion);
