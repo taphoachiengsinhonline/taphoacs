7a73e38e49f0d06c0e7b3f501f3ea01eb8068dde
taphoachiengsinhonline <maytinhthaikhang@gmail.com>
2026-04-14 23:42:03 +0700

    Update reviewRoutes.js

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -5,14 +5,22 @@ const reviewController = require('../controllers/reviewController');
 const { verifyToken } = require('../middlewares/authMiddleware');
 
 // Chỉ người dùng đã đăng nhập mới có thể tạo review
-router.get('/product/:productId', reviewController.getReviewsForProduct);
+//router.get('/product/:productId', reviewController.getReviewsForProduct);
 // <<< ROUTE MỚI >>>
-router.get('/status/:orderId', verifyToken, reviewController.getReviewStatusForOrder);
-router.post('/', verifyToken, reviewController.createReview);
+//router.get('/status/:orderId', verifyToken, reviewController.getReviewStatusForOrder);
+//router.post('/', verifyToken, reviewController.createReview);
 
+//router.get('/stats/:targetType/:targetId', reviewController.getRatingStats);
+//router.get('/rating-stats/:targetType/:targetId', reviewController.getRatingStats);
+//router.get('/shipper/:shipperId', reviewController.getReviewsForShipper);
+//router.get('/seller/:sellerId', reviewController.getReviewsForSeller);
+
+router.get('/product/:productId', reviewController.getReviewsForProduct);
+router.get('/shipper/:shipperId', reviewController.getReviewsForShipper);
+router.get('/seller/:sellerId', reviewController.getReviewsForSeller); // lên trước
+router.get('/status/:orderId', verifyToken, reviewController.getReviewStatusForOrder);
 router.get('/stats/:targetType/:targetId', reviewController.getRatingStats);
 router.get('/rating-stats/:targetType/:targetId', reviewController.getRatingStats);
-router.get('/shipper/:shipperId', reviewController.getReviewsForShipper);
-router.get('/seller/:sellerId', reviewController.getReviewsForSeller);
+router.post('/', verifyToken, reviewController.createReview);
 
 module.exports = router;
